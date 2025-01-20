2bcbb880d4e7de826245bc599e6d163e5a7a01fc
khiemhome <khiembi161219@gmail.com>
2025-01-20 13:11:35 +0700

    Layouts

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,16 @@
+export default function Layout({ children }) {
+    return (
+        <>
+            <header>
+                <nav>
+                    <Link className="nav-link" href="/">Home</Link>
+                    <Link className="nav-link" href="/create">Create</Link>
+                </nav>
+            </header>
+
+            <main>
+                { children }
+            </main>
+        </>
+    );
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,5 @@
+// import Layout from "@/Layouts/Layout.jsx";
+
 export default function Home({ name }) {
     return (
         <>
@@ -5,3 +7,7 @@ export default function Home({ name }) {
         </>
     );
 }
+
+// Home.layout = page => <Layout children={page} />;
+//
+// export default Home;
