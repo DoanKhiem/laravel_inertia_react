0f0598490d2d156ff8b261f40ae3f4b14afcd087
khiemhome <khiembi161219@gmail.com>
2025-01-20 14:38:49 +0700

    Model Preparation

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home', ['name' => 'Sadboiz IT']);
-});
+//Route::get('/', function () {
+//    return Inertia::render('Home', ['name' => 'Sadboiz IT']);
+//});
 
 //Route::inertia('/', 'Home');
 
-Route::get('/about', function () {
-    return Inertia::render('About/About');
-});
+//Route::get('/about', function () {
+//    return Inertia::render('About/About');
+//});
+
+Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+
+Route::resource('posts', \App\Http\Controllers\PostController::class)->except(['index']);
