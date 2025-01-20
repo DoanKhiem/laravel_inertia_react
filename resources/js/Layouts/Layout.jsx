fedd0bbbe8bef34914e13830bca5d51b2423ee9a
khiemhome <khiembi161219@gmail.com>
2025-01-20 13:16:48 +0700

    Scroll Position

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,3 +1,5 @@
+import { Link } from '@inertiajs/react'
+
 export default function Layout({ children }) {
     return (
         <>
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,14 @@
 // import Layout from "@/Layouts/Layout.jsx";
 
+import {Link} from "@inertiajs/react";
+
 export default function Home({ name }) {
     return (
         <>
             <h1 className="title">Hello, { name }</h1>
+
+
+            <Link preserveScroll href="/" className="block title mt-[1000px]" >{ new Date().toLocaleTimeString() }</Link>
         </>
     );
 }
