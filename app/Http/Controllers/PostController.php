fb619279f3e471648619fa9ac6cde2017ac58b7f
khiemhome <khiembi161219@gmail.com>
2025-01-21 13:33:07 +0700

    Create Resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -22,15 +23,22 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        $fields = $request->validate([
+            'body' => 'required'
+        ]);
+
+        Post::create($fields);
+
+        return redirect()->route('posts.index');
+
     }
 
     /**
